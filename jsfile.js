c998a729a8e40cc91e043b291b784117bf893d66
Antonio <atroncoso10@gmail.com>
2024-06-16 22:27:08 -0400

    Basic functionality of EaS with click instead of hover. To do: .- Need to fix validation for input .- review why class set attribute vs. event.target doesn't work

--- jsfile.js
+++ jsfile.js
@@ -2,23 +2,36 @@
 
 const inputGridSize = document.querySelector("#gridSize");
 const btnGridInput = document.querySelector("#btnG");
-
 const divContainerGrid = document.querySelector(".etchASketchContainer");
 
-
 const canvasSide = divContainerGrid.offsetWidth;
-let gridSize = 4;
-let tileWidth = Math.floor((canvasSide-16)/gridSize);
-console.log(canvasSide);
-console.log(tileWidth);
+// delete default of 4 after getting the hover correct
+let gridSize = 0;
+let tileWidth = 0;
 
 btnGridInput.addEventListener("click",(e)=>
 {
+    //limits gridsize to be between 10 and 1;
+    //captures value fron input
+    gridSize = inputGridSize.value;
+    if (gridSize>10 || gridSize < 2){
+        gridSize = 4;
+    }
+
+    tileWidth = (canvasSide-16)/gridSize;
     for (let i=1;i<=gridSize*gridSize;i++){
         divEtchASketch = document.createElement("div");
-        divEtchASketch.setAttribute("class","etchASketch");
-        divEtchASketch.setAttribute("style",`width:${tileWidth}px; height:${tileWidth}px; background: coral;`);
+        // divEtchASketch.setAttribute("class","etchASketch");
+        divEtchASketch.setAttribute("style",`width:${tileWidth}px; height:${tileWidth}px; background: #f4f4f4;`);
         divContainerGrid.appendChild(divEtchASketch);
+
+        divEtchASketch.addEventListener("click",(e)=> {
+            console.log(divEtchASketch);
+            console.log(e.target);
+            const divNewColor = e.target;
+            divNewColor.setAttribute("style",`width:${tileWidth}px; height:${tileWidth}px; background: #46444C;`);
+            // e.target.setAttribute("style",`width:${tileWidth}px; height:${tileWidth}px; background: #46444C;`);
+            })
         }
 })
 
