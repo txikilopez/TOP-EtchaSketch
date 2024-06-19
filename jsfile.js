8881994d0e6a7ffdf7dd855684571cd9e87218d3
Antonio <atroncoso10@gmail.com>
2024-06-18 22:01:53 -0400

    solves for extra credit. Additionally, breaks functions in smaller functions so that is cleaner and hopefully easier to read.

--- jsfile.js
+++ jsfile.js
@@ -1,32 +1,23 @@
-
 const inputGridSize = document.querySelector("#gridSize");
 const btnGridInput = document.querySelector("#btnG");
 const divContainerGrid = document.querySelector(".etchASketchContainer");
 const canvasSide = divContainerGrid.offsetWidth;
 let gridSize = 0;
 let tileWidth = 0;
+let opacity = 0; // delete if we don't want the opacity featuer
 
-
-btnGridInput.addEventListener("click",(e)=>
+btnGridInput.addEventListener("click",()=>
 {
     gridSize = getScreenSize();
-    tileWidth = (canvasSide-(16*2+2*2))/gridSize;
-
-    divContainerGrid.innerHTML='';
+    tileWidth = (canvasSide-(16*2+2*2))/gridSize; //16 margin size, 1px padding
+    divContainerGrid.innerHTML=''; // clears the etch a sketch grid
 
     for (let i=1;i<=gridSize*gridSize;i++){
-        divEtchASketch = document.createElement("div");
-        divEtchASketch.setAttribute("style",`width:${tileWidth}px; height:${tileWidth}px; background: #808080;`);
-        divContainerGrid.appendChild(divEtchASketch);
-        
-        divEtchASketch.addEventListener("mouseover",(e)=> {
-            const divNewColor = e.target;
-            divNewColor.style.background = "#5A5A5A";
-            // divEtchASketch.style.background = "#5A5A5A"; doesn't work
-            })
+        createCanvas(gridSize,tileWidth,i);
         }
 })
-//untested
+
+
 function getScreenSize(){
     let squares = +prompt("How big you want the grid to be (a x a)");
     if (typeof (squares) == "string" || !Number.isInteger(squares) || squares > 100 || squares <1){
@@ -34,4 +25,27 @@ function getScreenSize(){
         alert(`Positive integer under 100 expected, default grid of ${squares} x ${squares} created`);
         }
     return +squares;
+}
+
+function createCanvas(gridSizeInput,tiles,counter){
+    divEtchASketch = document.createElement("div");
+    divEtchASketch.setAttribute("style",`width:${tiles}px; height:${tiles}px; background: #808080;`);
+    // divEtchASketch.setAttribute("class","etchASketchPixel"+counter+" square");
+    divEtchASketch.style.opacity = 0; // delete if we dont' want the opacity feature
+    divContainerGrid.appendChild(divEtchASketch);      
+    
+    divEtchASketch.addEventListener("mouseover",(e)=> {
+        const divNewColor = e.target;
+        // const divNewColor = document.querySelector(`.etchASketchPixel${i}`)
+        let rColor = 255*(Math.random())+1;
+        let gColor = 255*(Math.random())+1;
+        let bColor = 255*(Math.random())+1;
+        divNewColor.style.background = `rgb(${rColor},${gColor},${bColor} )`;
+    },{once:true})
+    
+    divEtchASketch.addEventListener("mouseover",(e)=>{
+        const divNewColor = e.target;
+        opacity = +divNewColor.style.opacity;
+        if(opacity <1){divNewColor.style.opacity = opacity +0.1} 
+    })
 }
